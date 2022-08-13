89231409acb3274ed0f44f64d2d92b825bace5cf
JT\asp.mohammad.abuqaud <asp.mohammad.abuqaud@orange.com>
2022-08-13 18:09:50 +0300

    Initial commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,42 @@
+class UsersController < ApplicationController
+  skip_before_action :authenticate_request, only: [:create]
+  before_action :set_user, only: [:show, :destroy]
+
+  def index
+    @users = User.all
+    render json: @users, status: :ok
+  end
+
+  def show
+    render json: @users, status: :ok
+  end
+
+  def create
+    @users = User.new(user_params)
+    if @users.save
+      render json: @users, status: :created
+    else
+      render json: { error: @users.errors.full_message }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    unless @users.update(user_params)
+      render json: { error: @users.errors.full_message }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @users.destroy
+  end
+
+  private
+
+  def user_params
+    params.permit(:username, :email, :password)
+  end
+
+  def set_user
+    @users = User.find(params[:id])
+  end
+end
